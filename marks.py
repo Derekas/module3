c72e6013a83bc4f905daa8704495270218436af4
Derek <derekinformatica619@gmail.com>
2021-09-29 09:19:44 +0200

    First commit!

---
+++ marks.py
@@ -0,0 +1,12 @@
+n=float(input("Number 0-10 : "))
+
+if n>=9:
+    print("Excelent")
+elif n>=7:
+    print("Very Good")
+elif n==6:
+    print("Good")
+elif n==5:
+    print("Enough")
+else:
+    print("Fail")
